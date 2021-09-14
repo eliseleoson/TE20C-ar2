bf483d4ef824f122e80166b011d270f009103fe8
eliseleoson <leoson.elise@elev.ga.ntig.se>
2021-09-14 10:05:44 +0200

    Uppgift 1.4

---
+++ Uppgift 1.4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection.Emit;
+using System.Data;
+using System;
+
+namespace Uppgift_1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ändrar bakgrunden till blå
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+
+            //Ändrar texten till gul
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
