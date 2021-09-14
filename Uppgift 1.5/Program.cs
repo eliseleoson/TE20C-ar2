e39e4ea2b6154edcd7d33c8a57270f860236b7e7
eliseleoson <leoson.elise@elev.ga.ntig.se>
2021-09-14 10:07:55 +0200

    Uppgift 1.5

---
+++ Uppgift 1.5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Uppgift_1._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             // Skriver ut den första raden med text
+            Console.WriteLine("Du är nästan klar med kapitel 1.");
+
+            // Skriver ut den andra raden med text
+            Console.WriteLine("Du har lärt dig skriva ut tecken som \" och \\.");
+        }
+    }
+}
