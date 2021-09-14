a361df4ba88fb33351a1889419e31acd39e1e136
eliseleoson <leoson.elise@elev.ga.ntig.se>
2021-09-14 10:10:53 +0200

    Uppgift 1.6

---
+++ Uppgift 1.6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Uppgift_1._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+
+            Console.WriteLine("An error has ocurred.");
+            Console.WriteLine("Error: 0E : 016C : BFF415FA");
+        }
+    }
+}
