1bb93ebf1c53671135c22632831dde494fda8606
eliseleoson <leoson.elise@elev.ga.ntig.se>
2021-09-14 10:11:51 +0200

    Uppgift 1.7

---
+++ Uppgift 1.7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Uppgift_1._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("   /\\");
+            Console.WriteLine("  /  \\");
+            Console.WriteLine(" / \"\" \\");
+            Console.WriteLine("/______\\");
+        }
+    }
+}
