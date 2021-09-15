abf19d7f6170883369b7271b018fceb545de0720
eliseleoson <leoson.elise@elev.ga.ntig.se>
2021-09-15 11:09:35 +0200

    Lucktext

---
+++ Kapitel 2/Lucktext/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lucktext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Please write a place");
+            string place = Console.ReadLine();
+
+            Console.WriteLine("Please write a name");
+            string person = Console.ReadLine();
+            
+            Console.WriteLine("Please write a plural adjective(adjektiv)");
+            string adj1 = Console.ReadLine();
+            
+            Console.WriteLine("Please write a clothing item");
+            string clothing = Console.ReadLine();
+
+            Console.WriteLine("Please write a part of the body");
+            string bodypart = Console.ReadLine();
+            
+            Console.WriteLine($"The year was 1803 in {place}, as {person} entered the disco they saw a {adj1} fireman DJ wearing a complementary {clothing} on their {bodypart} ");
+            
+        }
+    }
+}
---
+++ Kapitel 2/Övning/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Övning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("Läser ett namn ");
+            string namn = Console.ReadLine();
+            Console.WriteLine($"Hej på dig {namn}");
+
+            Console.Write("Var bor du? ");
+            string ort = Console.ReadLine();
+            Console.WriteLine($"Vad trevligt att du bor i {ort}");
+
+            Console.Write("Hur gammal är du? ");
+            string ålder = Console.ReadLine();
+            Console.WriteLine($"Du är {ålder} år");
+        }
+    }
+}
