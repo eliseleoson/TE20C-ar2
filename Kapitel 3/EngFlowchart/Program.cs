1432e4499beecc761aab4752b8241bec375c500f
eliseleoson <leoson.elise@elev.ga.ntig.se>
2021-09-28 10:46:25 +0200

    Villkor

---
+++ Kapitel 3/EngFlowchart/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Data;
+using System;
+
+namespace EngFlowchart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Clear();
+            Console.WriteLine("Engineering Flowchart");
+
+            Console.WriteLine("Does it move? (y/n) ");
+            string answer1 = Console.ReadLine();
+            if (answer1 == "y")
+            {
+                Console.Write("Should it? (y/n) ");
+                string answer2 = Console.ReadLine();
+                if (answer2 == "y")
+                {
+                    Console.WriteLine("No problem! 😄");
+                }
+                else
+                {
+                    Console.WriteLine("Use ductape! 😊");
+                }
+            }
+            else
+            {
+                Console.Write("Should it? (y/n) ");
+                string answer3 = Console.ReadLine();
+                if (answer3 == "n")
+                {
+                    Console.WriteLine("No problem! ");
+                }
+                else
+                {
+                    Console.WriteLine("Use WD-40! ");
+                }
+            }
+        }
+    }
+}
---
+++ Kapitel 3/Interaktiv berättelse/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Interaktiv_berättelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Ooof! Du har hamnat i mörktunnel\nVad gör du? Gå fram eller bak?");
+            string answer1 = Console.ReadLine().ToLower();
+
+            if (answer1 == "fram")
+            {
+                Console.WriteLine("Du kommer fram till två dörrar, en svart och en grön. Vilken väljer du att ta? (Skriv färgen) ");
+                string answer2 = Console.ReadLine().ToLower();
+                if (answer2 == "svart")
+                {
+                    Console.WriteLine("Gudarna har dömt dig till en syndare och dödar dig!");
+                }
+                else
+                {
+                    Console.WriteLine("Du kommer fram till ett paradis med allt man kan tänka sig!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Du börjar se ett ljus i tunneln och kommer fram till en korsning. Du kan välja att gå höger eller vänster ");
+                string answer3 = Console.ReadLine().ToLower();
+                if (answer3 == "höger")
+                {
+                    Console.WriteLine("Right, lmao du kommer ut ur tunneln.\nDu tror att du är säker \nMEN FEL! En stor örn kommer och äter upp dig! ");
+                }
+                else
+                {
+                    Console.WriteLine("Du kommer ut ur tunneln och blir plötsligt påkörd av en buss! \nDu stämmer bussbolaget och chaffören och blir biljonär av rättegången!");
+                }
+            }
+        }
+    }
+}
