ae7bd85fee1e1274f79eb2764dad50a86c1a43e5
eliseleoson <leoson.elise@elev.ga.ntig.se>
2021-10-05 11:01:57 +0200

    Tränat på loopar

---
+++ Kapitel 4/GissaEttTal/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Gissa_ett_nummer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Spel - Gissa ett nummer 1-100");
+
+            // Slumpa fram ett tal
+            Random tärning = new Random ();
+            int slumptal = tärning.Next(1, 101);
+
+            // Upprepa frågan
+            int räknare = 0;
+            while (true)
+            {
+                // Räknar antal gissningar = varv
+                räknare++;
+
+                // Fråga användaren om en gissning
+                Console.Write("Gissa ett tal (1-100): ");
+                int gissning = int.Parse(Console.ReadLine());
+
+                // Är gissningen rätt?
+                if (gissning == slumptal)
+                {
+                    Console.WriteLine($"Bra gissat! Du hade rätt! Du gjorde det på {räknare} försök");
+                    break;
+                }
+                
+                // Jämför med slumptal
+                if (gissning < slumptal)
+                {
+                    Console.WriteLine("För lågt");
+                }
+                else
+                {
+                    Console.WriteLine("För högt");
+                }   
+
+            }
+        }
+    }
+}
---
+++ Kapitel 4/GissaEttTal2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace GissaEttTal2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Spelet Gissa ett tal!!");
+
+            // Slumpa fram ett tal
+            Random tärning = new Random ();
+            int slumptal = tärning.Next(1, 51);
+
+            // Loopa 5 gånger
+            int räknare = 0;
+            while (true)
+            {
+                // Räknar upp varv
+                räknare++;
+
+                // Bryt efter 5 varv
+                if (räknare >= 6)
+                {
+                    break;
+                }
+
+                // Fråga användaren om en gissning
+                Console.Write("Gissa ett tal (1-50): ");
+                int gissning = int.Parse(Console.ReadLine());
+
+                // Är gissningen rätt?
+                if (gissning == slumptal)
+                {
+                    Console.WriteLine($"Bra gissat! Du hade rätt! Du gjorde det på {räknare} försök");
+                    break;
+                }
+                
+                // Jämför med slumptal
+                if (gissning < slumptal)
+                {
+                    Console.WriteLine("För lågt");
+                }
+                else
+                {
+                    Console.WriteLine("För högt");
+                }   
+
+            }
+        }
+    }
+}
