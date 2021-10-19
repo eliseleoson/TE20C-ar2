f1940833aab1bafd917019be39b7f79e3dc7e36c
eliseleoson <leoson.elise@elev.ga.ntig.se>
2021-10-19 12:25:05 +0200

    For loopar

---
+++ Kapitel 4/Uppgift 4.4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Uppgift_4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Räkna från 40 till 80");
+            for (int i = 40; i < 81; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Kapitel 4/Uppgift 4.5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Uppgift_4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            for (int årtal = 1495; årtal >= 1400; årtal -= 5)
+            {
+                Console.WriteLine(årtal);
+            }
+        }
+    }
+}
