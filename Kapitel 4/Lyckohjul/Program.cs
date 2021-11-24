27962669ba66b34063b9906d1d8bb68015f6568c
eliseleoson <leoson.elise@elev.ga.ntig.se>
2021-11-24 10:20:15 +0100

    Kod

---
+++ Kapitel 4/Lyckohjul/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Lyckohjul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Välkommen till Lyckohjulet!");
+            
+            // Slumpa fram ett tal
+            Random slumpgenerator = new Random();
+            int slumptal = slumpgenerator.Next(1,11);
+            
+            // Frågar användaren om ett tal
+            Console.Write("Ange ett tal mellan 1-10 som du tror hjulet kommer stanna på, du har 3 försök: "); 
+            
+            // Räknar antal försök
+            int räknare = 0;
+            
+            while (true)
+            {
+                 räknare++;
+                
+                // Inte fler än 3 gissningar
+                if (räknare >= 4)
+                {
+                    break;
+                }
+
+                Console.Write("");
+                int gissning = int.Parse(Console.ReadLine());
+
+                // Om man gissar rätt
+                if (slumptal == gissning)
+                {
+                    Console.WriteLine($"Du gissade rätt! Svaret var {slumptal}");
+                    break;
+                }
+                
+                // Om man tar alla tre försök
+                if (räknare == 3)
+                {
+                    Console.WriteLine($"Du förlorade :( Svaret var {slumptal}"); 
+                }
+                else 
+                {
+                    Console.WriteLine("Testa igen");
+                }
+            
+            }
+                
+
+                
+           
+            
+            
+
+        }
+    }
+}
---
+++ Labbar/Labb1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Labb1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            // Presenterar programmet
+            Console.WriteLine("Program för Ceasar-kryptering");
+
+            // Ber användaren om ord
+            Console.Write("Skriv ett meddelande: ");
+            string ord = Console.ReadLine();
+            
+            // Frågar användaren hur många steg man ska hoppa 1-9
+            Console.Write("Ange en nyckel (1-9): ");
+            int nyckel = int.Parse(Console.ReadLine());
+            
+            string meddelandeKrypterad = "";
+
+            // Loopar ordet för varje bokstav
+            for (int i = 0; i < ord.Length; i++)
+            {
+                // Skriver bokstaven en i taget
+                char bokstav = ord[i];
+
+                // ASCII kod
+                int kod = (int)bokstav;
+
+                // Lägg till nyckeln
+                kod += nyckel;
+
+                // Tecken av ASCII-kod
+                char bokstavKrypterad = (char)(kod);
+
+                meddelandeKrypterad += bokstavKrypterad.ToString();
+
+                // Ceasar kryptering
+                Console.WriteLine($"{ord} blir {meddelandeKrypterad} ");
+
+            }
+        }
+    }
+}
