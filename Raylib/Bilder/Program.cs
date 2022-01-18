344aaed84241357bedff3a7d1e2c9b5f500b15f7
eliseleoson <leoson.elise@elev.ga.ntig.se>
2022-01-18 11:07:00 +0100

    Raylib

---
+++ Raylib/Bilder/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Raylib_cs;
+
+namespace Bilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Starta grafikmotorn
+            Raylib.InitWindow(1000, 800, "Mitt Raylib fönster");
+
+            // Bestäm skärmuppdatering
+            Raylib.SetTargetFPS(60);
+
+            // Ladda in bilden
+            Texture2D frog = Raylib.LoadTexture(@"./5845ec62dda95a5696fa1a27.png");
+            Texture2D elmo = Raylib.LoadTexture(@"./uploads_elmo_elmo_PNG90515.png");
+
+            // Animationsloopen
+            while (!Raylib.WindowShouldClose())
+            {
+                // Börja rita
+                Raylib.BeginDrawing();
+
+                // Töm ritytan
+                Raylib.ClearBackground(Color.DARKBLUE);
+
+                // Rita ut bilder
+                Raylib.DrawTexture(frog, 0, 0, Color.WHITE);
+                Raylib.DrawTexture(elmo, 200, 200, Color.WHITE);
+
+                // Ritat ut på fönstret
+                Raylib.EndDrawing();
+            }
+        }
+    }
+}
---
+++ Raylib/Input/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Raylib_cs;
+
+namespace Bilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Starta grafikmotorn
+            Raylib.InitWindow(800, 600, "Mitt Raylib fönster");
+
+            // Bestäm skärmuppdatering
+            Raylib.SetTargetFPS(60);
+
+            // Skapa en spelare av retangel
+            Texture2D avatar = Raylib.LoadTexture(@"./avatar.png");
+            Rectangle spelare = new Rectangle(100,100,50,50);
+            Texture2D warning = Raylib.LoadTexture(@"./warnings.png");
+            Rectangle fiende = new Rectangle(500,500,50,50);
+
+            
+            
+
+            // Animationsloopen
+            while (!Raylib.WindowShouldClose())
+            {
+                /* Rita ut grafiken */
+                // Börja rita
+                Raylib.BeginDrawing();
+
+                // Töm ritytan
+                Raylib.ClearBackground(Color.DARKBLUE);
+
+                // Rita ut spelaren
+                Raylib.DrawTexture(avatar, (int)spelare.x, (int)spelare.y, Color.WHITE);
+                Raylib.DrawTexture(warning, (int)fiende.x, (int)fiende.y, Color.WHITE);
+                
+                
+
+                // Ritat ut på fönstret
+                Raylib.EndDrawing();
+
+                /* Interaktion med användaren */
+                // Lyssna på tangentbordet
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+                {
+                    spelare.x +=5;
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+                {
+                    spelare.x -=5;
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN))
+                {
+                    spelare.y +=5;
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
+                {
+                    spelare.y -=5;
+                }
+
+                /* Fiende */
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
+                {
+                    fiende.x +=5;
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
+                {
+                    fiende.x -=5;
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_S))
+                {
+                    fiende.y +=5;
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_W))
+                {
+                    fiende.y -=5;
+                }
+            }
+        }
+    }
+}
+
